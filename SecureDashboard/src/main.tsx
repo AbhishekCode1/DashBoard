77332467f98725dc43901b5b371fe6e2cdca3eea
Abhishek Singh <109683906+AbhishekCode1@users.noreply.github.com>
2024-09-29 17:56:24 +0530

    update

--- SecureDashboard/src/main.tsx
+++ SecureDashboard/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import {createApp}  from 'vue';
+import { createPinia } from 'pinia';
+
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
+app.mount('#app');
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
